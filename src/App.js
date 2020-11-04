31167b0dc73480323cf2dd844b042369a9069218
Jorge Montes <jamaff86@gmail.com>
2020-11-04 09:16:46 -0500

    StyledComponents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import { useImperativeHandle } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, {StyleRoot} from 'radium';
+
+const StyledButton = styled.button`
+      background-color: green;
+      color: white;
+      font: inherit;
+      border: 1px solid blue;
+      padding: 8px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: lightgreen;
+        color: black
+      }
+`;
 
 const App = props => {
 
@@ -98,17 +112,14 @@ const App = props => {
     }
 
     return (
-      <StyleRoot>
       <div className="App">
         <h1>hi i'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style}
-          onClick={togglePersonsHandler}>Switch Name</button>
+        <StyledButton
+          onClick={togglePersonsHandler}>Switch Name</StyledButton>
         {persons}
       </div>
-      </StyleRoot>
     );
 };
 
-export default Radium(App);
+export default App;
